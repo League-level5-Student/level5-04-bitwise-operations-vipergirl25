a2582866ec8a15ed9cfb471c0ab91069741feb32
Elena <arayaleen@gmail.com>
2019-08-04 10:16:31 -0700

    ugh

--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Base64Decoder {
+	public static void main(String[] args) {
+		//convertBase64Char('e');
+		convert4CharsTo24Bits("////");
+	}
 	/*
 	 * Base 64 is a way of encoding binary data using text.
 	 * Each number 0-63 is mapped to a character. 
@@ -37,14 +41,48 @@ public class Base64Decoder {
 	//1. Complete this method so that it returns the the element in
 	//   the base64Chars array that corresponds to the passed in char.
 	public static byte convertBase64Char(char c){
-		return 0;
+		byte a = 0;
+		for (byte i = 0; i < base64Chars.length; i++) {
+			if(base64Chars[i] == c) {
+				a = i;
+			}
+		}
+	
+		return a;
 	}
 	
 	//2. Complete this method so that it will take in a string that is 4 
 	//   characters long and return an array of 3 bytes (24 bits). The byte 
 	//   array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s){
-		return null;
+		byte[] hey = {0b000000, 0b000000, 0b000000};
+		if(s.length()==4) {
+			for (int i = 0; i < hey.length; i++) {
+				String a = String.format("%8s", (Integer.toBinaryString(convertBase64Char(s.charAt(i))))).replace(' ', '0');
+				byte firstByte = (byte) Integer.parseInt(a);
+				System.out.println("A" + a);
+				String b = String.format("%8s", (Integer.toBinaryString(convertBase64Char(s.charAt(i+1))))).replace(' ', '0');
+				byte secondByte = (byte) Integer.parseInt(b);
+				System.out.println("B " + b);
+				//byte ab = (Integer.toBinaryString(s.charAt((convertBase64Char(s.charAt(i+1))))));
+				
+				//System.out.println("B" + ab);
+				secondByte = (byte)(secondByte<<4);System.out.println(secondByte);
+				byte secondByteShifted = (byte)Integer.parseInt(String.format("%8s", (Integer.toBinaryString(secondByte).replace(' ', '0'))));
+	
+				System.out.println("second byte " + Integer.toBinaryString(secondByte));
+				byte finalByte = (byte) (firstByte|secondByte);
+				System.out.println(Integer.toBinaryString(finalByte));
+				System.out.println("final byte " + finalByte);
+	
+				hey[i]=finalByte;
+				System.out.println("hey array " + hey[i]);
+				
+			}
+		}else{
+			return hey;
+		}
+		return hey;
 	}
 	
 	//3. Complete this method so that it takes in a string of any length
